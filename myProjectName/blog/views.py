f664d5bc4d40ac32896c42b06163f2ef3604672e
Turkevich91 <vetalturkevich@gmail.com>
2023-04-29 08:54:31 -0400

    include templates and dynamic context

--- myProjectName/blog/views.py
+++ myProjectName/blog/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
+from django.template.loader import render_to_string
 from django.http import HttpResponse
 
 
 # Create your views here.
 
 def blog_root_view(request):
-    return HttpResponse("<h1>Wellcome to my blog😁!</h1>")
+    rendered = render_to_string('blog_overview.html', {
+        'message': 'Wellcome to my blog😁!'
+    })
+    # return HttpResponse("<h1>Wellcome to my blog😁!</h1>")
+    return HttpResponse(rendered)
 
 
 def categories_view(request):
