0a9f2e07ebbaf1fc4e8c8ed878c38931b6e2474c
Turkevich91 <vetalturkevich@gmail.com>
2023-11-09 16:56:34 -0500

    Forms

--- myProjectName/blog/views.py
+++ myProjectName/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.template.loader import render_to_string
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views import View
 
 from blog.models import Animal
@@ -21,6 +21,14 @@ class AnimalDetailView(View):
     def get(self, request, animal_id):
         animal = get_object_or_404(Animal, id=animal_id)
         return render(request, self.template_name, {'animal': animal})
+
+    def post(self, request, animal_id):
+        animal = get_object_or_404(Animal, id=animal_id)
+        data = request.POST
+        name = data['name']
+        comment = data['comment']
+        print(name, 'left the comment on ', animal.name, 'page: ',  comment)
+        return HttpResponseRedirect(request.path)
 
 
 def blog_root_view(request):
